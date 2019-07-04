1fde18c7bc8efac37d725d400111afbaefa95b03
Srikanth Vemu <qalearnit@gmail.com>
2019-07-04 15:36:07 +0530

    test

---
+++ GitJavaRepository/src/javaExceptionHandling/JavaExceptionsDemo.java
@@ -0,0 +1,12 @@
+package javaExceptionHandling;
+
+public class JavaExceptionsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Exception handling is on the way");
+		
+	}
+
+}
