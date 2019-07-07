fbba9413de134608115b25d86dc12c4b4f3d0897
Srikanth Vemu <qalearnit@gmail.com>
2019-07-07 12:05:16 +0530

    number program

---
+++ GitJavaRepository/src/javaExceptionHandling/Numbers.java
@@ -0,0 +1,18 @@
+package javaExceptionHandling;
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10,i=0;
+		
+		while(i<=n)
+		{
+			System.out.println(i);
+			i++;
+		}
+		
+		
+	}
+
+}
