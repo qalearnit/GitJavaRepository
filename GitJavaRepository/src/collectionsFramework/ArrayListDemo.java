65e1e65c9f9efab9cc26c8a75a4efef8b91cb4a2
Srikanth Vemu <qalearnit@gmail.com>
2019-07-26 22:27:14 +0530

    some progms

--- GitJavaRepository/src/collectionsFramework/ArrayListDemo.java
+++ GitJavaRepository/src/collectionsFramework/ArrayListDemo.java
@@ -2,6 +2,10 @@ package collectionsFramework;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Iterator;
+
 
 public class ArrayListDemo {
 
@@ -16,6 +20,7 @@ public class ArrayListDemo {
 		alist.add(1);
 		alist.add(217);
 		
+		//Array List
 		System.out.println("Sorting an integer list ");
 		
 		System.out.println("Before the list is sorted");
@@ -24,6 +29,23 @@ public class ArrayListDemo {
 		System.out.println("After the list is sorted");
 		Collections.sort(alist);
 		System.out.println(alist);
+		
+		//Linked List
+		List<Integer> linkedList = new LinkedList<Integer>();
+		linkedList.add(50);
+		//linkedList.add(3,66);
+		linkedList.add(8);
+		linkedList.add(19);
+		linkedList.add(45);
+		
+//		System.out.println(linkedList);
+		
+		Iterator<Integer> itr = alist.iterator();
+		
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
 	}
 
 }
--- GitJavaRepository/src/collectionsFramework/SetDemo.java
+++ GitJavaRepository/src/collectionsFramework/SetDemo.java
@@ -1,18 +1,58 @@
 package collectionsFramework;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		//HashSet
 		Set<Integer> setOne = new HashSet<Integer>();
 		setOne.add(19);
+		setOne.add(9);
 		setOne.add(19);
+		setOne.add(188);
+		setOne.add(219);
+		
+		System.out.println("HashSet output is below ::>>");
 		
 		System.out.println(setOne);
+		
+		System.out.println("Order is not preserved with HashSet");
+		
+		//TreeSet
+		Set<Integer> setTwo = new TreeSet<Integer>();
+		setTwo.add(122);
+		setTwo.add(7);
+		setTwo.add(1999);
+		setTwo.add(93);
+		setTwo.add(5);
+		
+		System.out.println("TreeSet output is below ::>>");
+		
+		System.out.println(setTwo);
+		
+		System.out.println("The values are displayed in SortedOrder with TreeSet");
+		
+		
+		//LinkedHashSet
+		Set<Integer> setThree = new LinkedHashSet<Integer>();
+		setThree.add(122);
+		setThree.add(7);
+		setThree.add(1999);
+		setThree.add(93);
+		setThree.add(32);
+				
+		System.out.println("Linked HashSet output is below ::>>");
+		
+		System.out.println(setThree);
+		
+		System.out.println("Insertion Order is preserved with LinkedHashSet");
 	}
 
 }
